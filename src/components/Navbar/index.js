89b55c94ecfe640d3109eceff42abfe4c6340f04
paul88vera <paul88vera@gmail.com>
2022-11-30 22:40:17 -0600

    started homepage

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div id="navbar">
+      <img className="navbar-icon" src="/logo192.png" alt="" />
+      <img className="navbar-icon" src="/logo192.png" alt="" />
+      <div id="burger">
+        <img className="navbar-icon" src="/logo192.png" alt="" />
+      </div>
+    </div>
+  )
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
-    <div>
-      home
+    <div id='home'>
+      <Navbar />
+      <div id='main-banner'>
+        
+      </div>
     </div>
   );
 }
